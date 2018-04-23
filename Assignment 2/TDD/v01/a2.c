398a3ae25a9635979fa357d943e831b69a12859f
Ankit <ankit.jag04@gmail.com>
2018-04-23 15:45:35 +1000

    Assign 2 TDD/v01: Create Empty File working

---
+++ Assignment 2/TDD/v01/a2.c
@@ -0,0 +1,13 @@
+/*
+ v01 TDD: Input file is passed into program and output file is created
+*/
+
+#include "writeFile.h"
+
+// Function Prototypes
+
+int main (int argc, char argv)
+{
+	createFile ();
+	return 0;
+}
