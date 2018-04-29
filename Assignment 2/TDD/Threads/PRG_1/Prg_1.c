0fb28538f6c0ec52089d58cbdd3edd14ccfc73a1
Ankit <ankit.jag04@gmail.com>
2018-04-30 06:22:17 +1000

    Input file values passed to Output file via soft pipe

--- Assignment 2/TDD/Threads/PRG_1/Prg_1.c
+++ Assignment 2/TDD/Threads/PRG_1/Prg_1.c
@@ -22,7 +22,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define BUFFER_SIZE 256
-#define NUM_LINES 15 /* DETERMINE THIS BY READING FILE */
+#define NUM_LINES 15 /* End Header on Line 10*/ /* DETERMINE THIS BY READING FILE */
 
 /*!
  * @brief Checks if file to read is specified
@@ -30,7 +30,7 @@
  * @param argc the inputs to the program
  * @return 1 - if file not specified; 0 if correct
 */
-int checkInput (int argc) 
+int checkInput (int argc)
 {
   int input = argc;
   if (input != 2) 
@@ -51,7 +51,9 @@ int checkInput (int argc)
 /*!
  * @brief Creates file with unique filename bsed
  * 
- * @param argc the inputs to the program
+ * @param fileWrite contents to be written to the file
+ * @param mypipefd pipe to read from
+ * @param f file to write to
 */
 void writeFile (char * fileWrite, int * mypipefd, FILE * f) 
 {
@@ -81,10 +83,8 @@ void readLine (char ** argv)
   //if File exists, store in buffer
   if (file != NULL) 
 	{
-    printf ("==Line Read==:\n");
   //read one line from the text file.
     fgets (lineRead, BUFFER_SIZE, file);
-    printf ("%s", lineRead);
   }
 }
 
@@ -135,6 +135,7 @@ int main (int argc, char * * argv)
     FILE * file = fopen (argv [1], "r");
     char pipeWrite [BUFFER_SIZE];
     char pipeRead [BUFFER_SIZE];
+    char fileWrite [BUFFER_SIZE];
     char ch;
     //if File exists, store in buffer
     if (file != NULL) 
@@ -142,30 +143,69 @@ int main (int argc, char * * argv)
       for (int i = 0; i < NUM_LINES; i++) 
 			{
         //read one line from the text file.
-        printf ("==Pipe INPUT==\n\n");
-        //Check what is being written to pipe
         fgets (pipeWrite, BUFFER_SIZE, file);
-        printf ("%s", pipeWrite);
+        //Check what is being written to pipe
+        printf ("pipeWrite = %s\n", pipeWrite);
         // write buffer contents to pipe
         write (mypipefd[1], pipeWrite, strlen(pipeWrite));
-        // close pipe input
+/*        // close pipe input
         close (mypipefd[1]);
         // signal Thread B
 
-        /* Thread B */
+        /* Thread B  */
         // read buffer contents to buffer
-        read (mypipefd [0], pipeRead, BUFFER_SIZE);
+        //read (mypipefd [0], pipeRead, BUFFER_SIZE);
         //Check what is being read from pipe
-        printf ("\n==Pipe OUTPUT==\n");
-        printf ("%s", pipeRead);
+        //printf ("pipeRead = %s\n", pipeRead);
         // Signal Thread C
-      
 
       /* Thread C */
 
-      /*const*/ char * fileWrite = pipeWrite;
+      size_t fwSize = sizeof (fileWrite);
+      strncpy (fileWrite,pipeWrite, fwSize);
 
-      writeFile (fileWrite, mypipefd, f);
+/*       char end[BUFFER_SIZE];
+
+       char endText[BUFFER_SIZE] = "end_header\0";
+
+       size_t sizeEnd = sizeof(endText);
+			
+      int endDetect = strncmp (fileWrite, endText, sizeof (endText));
+
+      int endComp = strncmp (endLine, fileWrite, sizeof (fileWrite));
+
+      switch (endComp)
+      {
+      case -999 ... -1 :
+        printf ("endLine < fileWrite\n");
+      break;
+      case 0 :
+        printf ("endLine = fileWrite\n");
+      break;
+      case 1 ... 999 :
+        printf ("endLine > fileWrite\n");
+      break;
+      default: 
+        printf ("No Comparison\n");
+      }
+      
+  */    //char end = (char) (strcmp(fileWrite, "end_header") == 0) ? "True" : "False" ;
+
+      //printf ("pipeRead = %s\n", pipeRead);
+
+			printf ("fileWrite = %s\n", fileWrite);
+			
+			//printf ("strncmp = %d\n", strcmp(endText, fileWrite));//, sizeEnd));
+
+      //printf ("end = %s\n", end);
+
+      //printf ("strncmp(fileWrite, end, sizeEnd) = %d\n", endDetect);
+		  writeFile (fileWrite, mypipefd, f);
+			//Raise Flag When end_header detected
+      if(1) //(endDetect == 0)
+			{
+		
+			}
 			}
       // Signal Thread A
     }
