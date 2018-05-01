c087c8e58955f5999a22d7d6841a9904a021fb6f
Ankit <ankit.jag04@gmail.com>
2018-05-01 10:54:17 +1000

    a

--- Assignment 2/TDD/Threads/PRG_1/Prg_1.c
+++ Assignment 2/TDD/Threads/PRG_1/Prg_1.c
@@ -4,7 +4,7 @@
  */
 
 /*!
- * @file Prg_1.c
+ * @inputFile Prg_1.c
  * 
  * @brief 
  * Compile Using: 
@@ -22,22 +22,37 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+
 #define BUFFER_SIZE 200
-#define NUM_LINES 15 /* Total Lines 15 */ /* End Header on Line 10*/ /* DETERMINE THIS BY READING FILE */
+#define NUM_LINES 15 /* Total Lines 15 */ /* End Header on Line 10*/ /* DETERMINE THIS BY READING inputFILE */
+
+/* Global Variables */
+
+struct file_runner_struct
+{
+  FILE * inputFile;
+  FILE * outputFile;
+  //pipe 
+  char pipeWrite[BUFFER_SIZE];
+  char pipeRead[BUFFER_SIZE];
+  char fileWrite[BUFFER_SIZE];
+  char buffer[BUFFER_SIZE];
+};
 
 /*!
- * @brief Checks if file to read is specified
+ * @brief Checks if inputFile to read is specified
  * 
  * @param argc the inputs to the program
- * @return 1 - if file not specified; 0 if correct
+ * @return 1 - if inputFile not specified; 0 if correct
  */
 int checkInput(int argc) {
   int input = argc;
   if (input != 2) {
     printf("The program should have one input parameter,\n");
     printf("the parameter is the document to be read.\n");
-    printf("Usage: exe text_file_path\n");
-    printf("Example: ./ txt_file\n");
+    printf("Usage: exe inputtext_File_path\n");
+    printf("Example: ./ inputtxt_File\n");
 
     return 1;
   } else {
@@ -46,37 +61,43 @@ int checkInput(int argc) {
 }
 
 /*!
- * @brief Creates file with unique filename bsed
+ * @brief Creates inputFile with unique inputFilename bsed
  * 
- * @param fileWrite contents to be written to the file
+ * @param fileWrite contents to be written to the inputFile
  * @param mypipefd pipe to read from
- * @param f file to write to
+ * @param f inputFile to write to
  */
 void writeFile(char * fileWrite, int * mypipefd, FILE * f) {
   if ( * mypipefd == -1) {
-    printf("Failed to create and open the file.\n");
+    printf("Failed to create and open the inputFile.\n");
     exit(1);
   } else {
-    // Write contents to file
+    // Write contents to inputFile
     fprintf(f, fileWrite, f);
   }
 }
 
 /*!
- * @brief Read one line of text from a file
+ * @brief Read one line of text from a inputFile
  * 
  * @param argv the inputs to the program
  */
-void readLine(char * * argv) {
-  char * * argvinput = argv;
-  FILE * file = fopen(argv[1], "r");
+void * readLine(void * arg) 
+{
+  char * * argvinput = arg;
+  
+  FILE * inputFile = fopen(arg, "r");
+
   char lineRead[BUFFER_SIZE];
   char ch;
-  //if File exists, store in buffer
-  if (file != NULL) {
-    //read one line from the text file.
-    fgets(lineRead, BUFFER_SIZE, file);
+  
+  //if inputFile exists, store in buffer
+  if (inputFile != NULL) {
+    //read one line from the text inputFile.
+    fgets(lineRead, BUFFER_SIZE, inputFile);
   }
+
+  return lineRead;
 }
 
 /*!
@@ -88,88 +109,108 @@ void writePipe() {
 }
 
 int main(int argc, char * * argv) {
-    /* Check for input text file */
+    /* Check for input text inputFile */
     if (checkInput(argc)) {
-      printf("\nNo input file to read!\n");
+      printf("\nNo input inputFile to read!\n");
       return 1;
     } 
     else 
     {
       /* Initialise Functions */
       //		int mypipefd;
+
+      struct file_runner_struct FileData;
       time_t rawtime;
-      char buffer[255];
       time( & rawtime);
 
       /* Create Pipe */
       int mypipefd[2];
       pipe(mypipefd);
 
-      // create file with Unique eName /* SAVEFILE NAME GETS CHANGED HERE  ==IGNORE DYNAMIC FILE WARNINGS FOR THIS==*/
-      const char * namedFile = sprintf(buffer, "GenFile-%s.txt", ctime( & rawtime));
+      // create inputFile with Unique eName /* inputSAVEFILE NAME GETS CHANGED HERE  ==IGNORE DYNAMIC inputFILE WARNINGS FOR THIS==*/
+      const char * inputnamedFile = sprintf (FileData.buffer, "inputGenFile-%s.txt", ctime( & rawtime));
 
-      // open a text file and print to screen
-      FILE * f = fopen(buffer, "w");
+      // open a text inputFile and print to screen
+      FileData.inputFile = fopen(FileData.buffer, "w");
 
       /* Create Signal Variables */
 
       /* Create Threads */
+      // Thread IDs
+      pthread_t tidA;
+      pthread_t tidB;
+      pthread_t tidC;
+
+      // Create Attributes
+      pthread_attr_t attrA;
+      pthread_attr_t attrB;
+      pthread_attr_t attrC;
+
+      pthread_attr_init (&attrA);
+      pthread_attr_init (&attrB);
+      pthread_attr_init (&attrC);
 
       /* Thread A */
 
       /* Read Input */
-      char * * argvinput = argv;
-      FILE * file = fopen(argv[1], "r");
-      char pipeWrite[BUFFER_SIZE];
-      char pipeRead[BUFFER_SIZE];
-      char fileWrite[BUFFER_SIZE];
+/*      char * * argvinput = argv;
+      inputFILE * inputFile = fopen(argv[1], "r");
+
+*/      pthread_create (&tidA, &attrA, readLine, &argv);
+
+// Wait until thread is done
+
+        pthread_join (tidA, NULL);
+
+printf ("Reaches pipeWrite\n");
       char ch;
 
       char endText[BUFFER_SIZE] = "end_header";
 
-      char needle[BUFFER_SIZE] = "end_header";
+      char phraseToFind[BUFFER_SIZE] = "end_header";
       char * ret = 0;
       int headerFlag = -1;
-      //if File exists, store in buffer
-      if (file != NULL) {
+      //if inputFile exists, store in buffer
+      if (FileData.inputFile != NULL) {
         for (int i = 0; i < NUM_LINES; i++) 
         {
-          //read one line from the text file.
-          fgets(pipeWrite, BUFFER_SIZE, file);
-          write(mypipefd[1], pipeWrite, strlen(pipeWrite));
+          //read one line from the text inputFile.
+          fgets(FileData.buffer, BUFFER_SIZE, FileData.inputFile);
+          write(mypipefd[1], FileData.pipeWrite, strlen(FileData.pipeWrite));
 
           /* Thread B Read from pipe to pipeRead */
 
           /* Thread C Copy to Buffer and Filter out Header */
 
-          size_t fwSize = sizeof(fileWrite)/ sizeof (int);
+          size_t fwSize = sizeof(FileData.fileWrite)/ sizeof (int);
           size_t etSize = sizeof(endText)/ sizeof (int);
-          strncpy(fileWrite, pipeWrite, etSize);
+          strncpy(FileData.fileWrite, FileData.pipeWrite, etSize);
 
           // Raise a flag if end detected
-            ret = strstr(fileWrite, needle);
+            ret = strstr(FileData.fileWrite, phraseToFind);
 
-          // Write to file if flag raised
+          // Write to inputFile if flag raised
             if (ret != 0 || headerFlag != -1)
             {
-              headerFlag++; 
+              headerFlag++;
+              printf ("end_header found!");
             }
 
             else if (ret != 0)
             {
               printf ("Found end_header at %d\n", i);
             }
-            /* This is what gets Printed To The Output File */
+            /* This is what gets Printed To The Output inputFile */
             if (headerFlag > 0)
             {
-              writeFile(fileWrite, mypipefd, f);
-              printf ("%s\n",fileWrite);
+              writeFile(FileData.fileWrite, mypipefd, FileData.outputFile);
+              printf ("%s\n",FileData.fileWrite);
             }
               
             // printf ("ret = %d\n", ret);
             // printf ("headerFlag = %d\n", headerFlag);
 
-          int endMatch = strncmp (endText, fileWrite, etSize);
+          int endMatch = strncmp (endText, FileData.fileWrite, etSize);
         }
 
       }
